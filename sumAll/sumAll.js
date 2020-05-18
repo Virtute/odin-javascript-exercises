35161550f039d146fe13e0d8dfc87c1634391680
Duncan Boatright <jdboatright@gmail.com>
2020-05-18 14:54:27 -0400

    Solved sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
-}
+const sumAll = function(first,second) {
+  var finalSum = 0;
+  if ( first < 0 || second < 0 || typeof first != "number" || typeof second != "number" ) {
+    return "ERROR";
+  };
+  if ( first < second ) {
+    var small = first;
+    var big = second;
+  } else {
+    var small = second;
+    var big = first;
+  };
+  for (let i=small;i<=big;i++) {
+    finalSum += i;
+  };
+  return finalSum;
+};
 
 module.exports = sumAll
